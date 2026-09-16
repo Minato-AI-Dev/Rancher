22de3da5f478c74beafcf48b91973c4d07e50b72
みなと <love.app.develper@gmail.com>
2026-09-16 12:24:36 +0900

    core-model: configure module

---
+++ core-model/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "dev.rancher.core.model"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
